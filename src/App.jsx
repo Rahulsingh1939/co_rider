72fc48f2f40ce6a65dd87a470b4d6464e15f77ef
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-05-24 11:38:45 +0530

    removing Template codes

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,7 @@ function App() {
   return (
     <>
       
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-      </div>
+     
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
